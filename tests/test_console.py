ff689f73dcf07cc9591737ed061cea99c4464dec
Herman100 <hermankwamebour30@gmail.com>
2023-07-15 23:51:14 +0000

    test file updated

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,7 @@ import unittest
 from console import HBNBCommand
 from io import StringIO
 import sys
+from unittest.mock import patch
 
 
 class TestConsole(unittest.TestCase):
@@ -28,6 +29,59 @@ class TestConsole(unittest.TestCase):
         hbnb.onecmd('\n')
         self.assertEqual(output.getvalue(), '')
         sys.stdout = sys.__stdout__
+
+    def test_create(self):
+        """Test the create command"""
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("create")
+            self.assertEqual(output.getvalue().strip(),
+                             "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("create MyModel")
+            self.assertEqual(output.getvalue().strip(),
+                             "** class doesn't exist **")
+
+    def test_show(self):
+        """Test the show command"""
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("show")
+            self.assertEqual(output.getvalue().strip(),
+                             "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("show BaseModel")
+            self.assertEqual(output.getvalue().strip(),
+                             "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("show MyModel 1234-5678-9012")
+            self.assertEqual(output.getvalue().strip(),
+                             "** class doesn't exist **")
+
+    def test_destroy(self):
+        """Test the destroy command"""
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("destroy")
+            self.assertEqual(output.getvalue().strip(),
+                             "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("destroy BaseModel")
+            self.assertEqual(output.getvalue().strip(),
+                             "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("destroy MyModel 1234-5678-9012")
+            self.assertEqual(output.getvalue().strip(),
+                             "** class doesn't exist **")
+
+    def test_all(self):
+        """Test the all command"""
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd("all MyModel")
+            self.assertEqual(output.getvalue().strip(),
+                             "** class doesn't exist **")
 
 
 if __name__ == '__main__':
