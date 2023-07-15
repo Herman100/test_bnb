d43d11b56eee206d9d15f6b8b2c3d804a67e2e83
Herman100 <hermankwamebour30@gmail.com>
2023-07-15 19:44:16 +0000

    the console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a custom cmd module that contains the
+entry point of the command interpreter"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
