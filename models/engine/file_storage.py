389c121d85376eec2d5dcadef6aa0dea3549ef11
Herman100 <hermankwamebour30@gmail.com>
2023-07-16 01:52:36 +0000

    updated models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
@@ -17,8 +18,9 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
@@ -37,6 +39,7 @@ class FileStorage:
                 json_dict = json.load(f)
             for key, value in json_dict.items():
                 class_name = value["__class__"]
-                FileStorage.__objects[key] = eval(class_name)(**value)
+                if class_name in ["BaseModel", "User"]:
+                    FileStorage.__objects[key] = eval(class_name)(**value)
         except FileNotFoundError:
             pass
